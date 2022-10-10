7f59d8b34dca213177c3b9219fcbab18fcee7a9a
bsc-com-27-19 <bsc-com-27-19@unima.ac.mw>
2022-10-10 11:17:32 -0700

    Authorizing only admin to disable an account

--- src/main/java/com/spring/security/demo/user/UserController.java
+++ src/main/java/com/spring/security/demo/user/UserController.java
@@ -56,6 +56,7 @@ public class UserController {
     }
 
     @PutMapping("disable-account/{userName}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ApiResponse> disableAccount(@PathVariable("userName") String userName){
         return userService.disableAccount(userName);
     }
